8e5abe95d40669afc87c2777ce78767c6027068b
Darren Embry <dse@webonastick.com>
2019-07-07 18:37:38 -0400

    comments

--- wear/src/main/java/com/webonastick/watchface/pilotwatch/PilotWatchFace.java
+++ wear/src/main/java/com/webonastick/watchface/pilotwatch/PilotWatchFace.java
@@ -47,28 +47,17 @@ import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Analog watch face with a ticking second hand. In ambient mode, the second hand isn't
- * shown. On devices with low-bit ambient mode, the hands are drawn without anti-aliasing in ambient
- * mode. The watch face is drawn with less contrast in mute mode.
- * <p>
- * Important Note: Because watch face apps do not have a default Activity in
- * their project, you will need to set your Configurations to
- * "Do not launch Activity" for both the Wear and/or Application modules. If you
- * are unsure how to do this, please review the "Run Starter project" section
- * in the Google Watch Face Code Lab:
- * https://codelabs.developers.google.com/codelabs/watchface/index.html#0
- */
 public class PilotWatchFace extends CanvasWatchFaceService {
 
     /*
-     * Updates rate in milliseconds for interactive mode. We update once a second to advance the
-     * second hand.
+     * Updates rate in milliseconds for interactive mode. We update
+     * once a second to advance the second hand.
      */
     private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
 
     /**
-     * Handler message id for updating the time periodically in interactive mode.
+     * Handler message id for updating the time periodically in
+     * interactive mode.
      */
     private static final int MSG_UPDATE_TIME = 0;
 
@@ -1065,12 +1054,15 @@ public class PilotWatchFace extends CanvasWatchFaceService {
                 if (stopwatchRunning) {
                     mUpdateRateMs = 50;
                 } else {
+                    // seconds for time of day is on main dial.
                     mUpdateRateMs = 200;
                 }
             } else {
                 if (stopwatchRunning) {
                     mUpdateRateMs = 50;
                 } else {
+                    // seconds for time of day is on subdial; that
+                    // updates once per second.
                     mUpdateRateMs = 1000;
                 }
             }
