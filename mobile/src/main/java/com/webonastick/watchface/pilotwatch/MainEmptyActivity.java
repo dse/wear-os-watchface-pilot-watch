cc62ffc207f7b3a7c69420571d5f214f8bb2b387
Darren Embry <dse@webonastick.com>
2019-07-07 15:22:21 -0400

    mobile component for installing on Android Wear 1.x

---
+++ mobile/src/main/java/com/webonastick/watchface/pilotwatch/MainEmptyActivity.java
@@ -0,0 +1,13 @@
+package com.webonastick.watchface.pilotwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_empty);
+    }
+}
